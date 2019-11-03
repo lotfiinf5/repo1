8d6b890657d4b8fbdcc8521a443732021030848e
User <User@DESKTOP-E2R42SK>
2019-11-02 21:01:48 -0400

    comit 1

--- javaprojectrepo01/src/com/lotfi/frm/Main.java
+++ javaprojectrepo01/src/com/lotfi/frm/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test 0001");
 		System.out.println("Test 0002");
-		System.out.println("Test 0003");
+		System.out.println("Test 0004");
 		
 		
 		
