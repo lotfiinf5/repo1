4e057ba81afa945f23c186dde60f811471b696a6
User <User@DESKTOP-E2R42SK>
2019-11-02 21:02:52 -0400

    commit

--- javaprojectrepo01/src/com/lotfi/frm/Main.java
+++ javaprojectrepo01/src/com/lotfi/frm/Main.java
@@ -2,14 +2,13 @@ package com.lotfi.frm;
 
 public class Main {
 
+	/**
+	 * main method
+	 */
 	public static void main(String[] args) {
 		System.out.println("Test 0001");
 		System.out.println("Test 0002");
 		System.out.println("Test 0004");
-		
-		
-		
 	}
-	
 
 }
